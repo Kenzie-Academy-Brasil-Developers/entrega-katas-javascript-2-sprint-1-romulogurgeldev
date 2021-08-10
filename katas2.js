19cd081e092e7309dec4f3f653d167bce87c7bad
Romulo Gurgel <rgurgel.romulo@gmail.com>
2021-08-10 15:29:23 -0300

    entrega katas-10-08

--- katas2.js
+++ katas2.js
@@ -1,29 +1,41 @@
 // comece a criar a sua função add na linha abaixo
-
+function add(a,b){
+    return (a + b);
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+ console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
+function multiply(a,b){
+    return (a *b);
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+ console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-
+function power(a,b){
+    return (a**b);
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+ console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+function factorial(a){
+    let meuRetorno = a;
+    for(let i = 1; i < a; i++){
+            meuRetorno = meuRetorno *i;
+    }
+    return meuRetorno;
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+ console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
